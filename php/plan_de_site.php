03a7b58fb6e9b5996ddecfa31339a316cbca17be
AmelieAlonah <amelie.abdallah@hotmail.fr>
2021-07-11 15:55:53 +0200

    Page plan

---
+++ php/plan_de_site.php
@@ -0,0 +1,14 @@
+
+<?php include "inc/header.php"; ?>
+
+  <h2 class="right__title">Plan de site</h2>
+  <div class="posts">
+    <div class="post post--solo">
+      <p class="single">
+        TODO
+      </p>
+      <a href="./index.html" class="post__link">back to home</a>
+    </div>
+  </div>
+
+<?php include "inc/footer.php"; ?>
